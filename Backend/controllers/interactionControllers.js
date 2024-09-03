f27383cc55aa8d0f13630965ffa176f1d02db8df
Fernando Castillo <fernacastillo.perez0607@gmail.com>
2024-09-03 16:32:19 -0400

    get soli

--- Backend/controllers/interactionControllers.js
+++ Backend/controllers/interactionControllers.js
@@ -7,3 +7,15 @@ export const getPatagonData = (req, res) => {
         res.status(500).send("Internal Server Error");  
     }
 };
+
+
+export const getSolicitudes = (req, res) => {
+    try{
+        console.log("solicitues usuarios");
+        res.status(200).send("Solicitudes de usuarios");
+    }
+    catch (error) {
+        console.error("Error", error),
+        res.status(500).send("Error de solicitud");
+    }
+};
--- Backend/routes/interactionRoute.js
+++ Backend/routes/interactionRoute.js
@@ -1,14 +1,15 @@
 import express from "express";
 
 import{
-    getPatagonData
+    getPatagonData,
+    getSolicitudes
 } from "../controllers/interactionControllers.js"
-
 
 
 const router = express.Router();
 
-
 router.get("/", getPatagonData);
+router.get("/solicitudes", getSolicitudes)
+
 
 export {router as interactionRouter}
