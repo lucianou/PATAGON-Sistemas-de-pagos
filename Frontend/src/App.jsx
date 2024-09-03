103e2b2bb739452620ed3cc5dc8dd47dcd507d41
Vicntea <128265139+Vicntea@users.noreply.github.com>
2024-09-03 16:29:23 -0400

    Están todas las páginas para trabajar.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Pages/Login';
 import Dashboard from './Pages/Dashboard';
+import Dashboard_solicitudes from './Pages/Dashboard_solicitudes';
 import Dashboard_user from './Pages/Dashboard_user'; // Importa el nuevo componente
+import Dashboard_profit from './Pages/Dashboard_profit';
+import Dashboard_config from './Pages/Dashboard_config';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard-solicitudes" element={<Dashboard_solicitudes />} />
+        <Route path="/dashboard-profit" element={<Dashboard_profit />} />
+        <Route path="/dashboard-config" element={<Dashboard_config />} />
         <Route path="/dashboard-user" element={<Dashboard_user />} /> {/* Nueva ruta */}
       </Routes>
     </Router>
---
+++ Frontend/src/Pages/Dashboard_profit.jsx
@@ -0,0 +1,13 @@
+// Pages/Dashboard_user.jsx
+import React from 'react';
+
+const Dashboard_profit = () => {
+  return (
+    <div>
+      <h1>Dashboard ganancias</h1>
+      <p>Welcome to the user dashboard!</p>
+    </div>
+  );
+};
+
+export default Dashboard_profit;
