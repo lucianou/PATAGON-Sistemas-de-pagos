f10d5937f481eb5a98382c1c21daf4b8a30fb17a
Lukayx <olavarriafernando36@gmail.com>
2024-09-03 16:38:29 -0400

    index modificado al normal

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import "../styles/Login.css";
 
 function Login() {
   const navigate = useNavigate();
@@ -9,11 +10,23 @@ function Login() {
   };
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button onClick={handleLogin}>Login</button>
+    <div className="login-container">
+      <h1 className="h1_patagon">Pata<span>gón</span></h1>
+      <div className="div_login">
+        <h1 className="h1_login">Login</h1>
+        <div className="form-group1">
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div className="form-group2">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <div className='form-group3'>
+          <button type="submit">Submit</button>
+          <button type="sign_-in">Sign in</button>
+        </div>
+      </div>
     </div>
   );
 }
