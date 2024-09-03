7fd6eeb409e2dcb7349d6c1303a4595693f23dd9
Vicntea <128265139+Vicntea@users.noreply.github.com>
2024-09-03 15:59:50 -0400

    Agregué dashboards

--- Frontend/src/Pages/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import '../styles/Dashboard.css'; // Para estilos personalizados
 
-function Dashboard() {
+const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to your dashboard!</p>
+    <div className="dashboard-container">
+      <aside className="sidebar">
+        <div className="profile">
+          <div className="profile-pic"></div>
+          <h2>Admin_name</h2>
+        </div>
+        <nav className="menu">
+          <ul>
+            <li>Dashboard</li>
+            <li>Solicitudes</li>
+            <li>Usuarios</li>
+            <li>Ganancias</li>
+            <li>Configuración</li>
+            <li>Cerrar sesión</li>
+          </ul>
+        </nav>
+      </aside>
+      <main className="content">
+        <div className="dashboard-header">
+          <h1>Dashboard</h1>
+        </div>
+        <div className="dashboard-widgets">
+          <div className="widget small-widget"></div>
+          <div className="widget small-widget"></div>
+          <div className="widget large-widget"></div>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default Dashboard;
