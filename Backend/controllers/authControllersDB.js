b4e37449631af66bdd14b662e4f8a8fef967b6f3
Fernando Castillo <fernacastillo.perez0607@gmail.com>
2024-09-16 12:39:47 -0300

    registro e inicio base de datos

--- Backend/controllers/authControllersDB.js
+++ Backend/controllers/authControllersDB.js
@@ -44,3 +44,43 @@ export async function loginUserDB(req, res) {
         res.status(500).json({ error: "Error al iniciar sesión" });
     }
 }
+
+//RRGISTRO EN BASE DATOS
+export async function register(req, res) {
+    const { email, password, username } = req.body;
+    if (!email || !password || !username) {
+        return res
+          .status(400)
+          .json({ error: "Email, usuario y contraseña son requeridos" });
+    }
+
+    try{
+        const checkUserQuery = 'SELECT * FROM public."Users" WHERE email = $1';
+        const checkUserResult = await pool.query(checkUserQuery, [email]);
+
+        if(checkUserResult.rows.length > 0){
+            return res.status(400).json({error: "El correo ya esta registrado"});
+        }
+
+        //encriptación
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //INSERTAR NUEVO USUARIO
+        const insertUserQuery = ` INSERT INTO users (email, password, username) VALUES ($1, $2, $3) RETURNING id, email, username; `;
+        const insertUserValues = [email, hashedPassword, username];
+
+
+
+        const newUserResult = await pool.query(insertUserQuery, insertUserValues);
+        const newUser = newUserResult.rows[0];
+
+        res.status(201).json({
+            message:"Usuario registrado correctamente",
+            user: newUser,
+        });
+    } catch(error){
+        console.log(error);
+        res.status(500).json({error:"Erro al registrar usuario"});
+    }
+    
+}
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -6,13 +6,15 @@ import {
 } 
 from "../controllers/authControllers.js";
 
-import{loginUserDB} from '../controllers/authControllersDB.js'
+import{loginUserDB, register} from '../controllers/authControllersDB.js'
 const router = express.Router();
 
 
 router.post("/userLog", loginUser);
 router.post("/userReg", registerUser);
 
+//LOGIN Y REGISTRO EN BASE DE DATOS
 router.post("/userLogDB", loginUserDB);
+router.post("/register", register);
 
 export {router as AuthRouter}
