bdaebd27a5df26ee0690113d5a145e72bf836899
Lukayx <olavarriafernando36@gmail.com>
2024-09-18 00:16:05 -0300

    Cambios agregados al dashboard, Fijarse en componente menuDashboard (se agrego un parametro)

--- Frontend/public/Components/menuDashboard/menuDashboard.jsx
+++ Frontend/public/Components/menuDashboard/menuDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom'; // Importa useLocation para obtener la URL actual
-import './MenuDashboard.css'; // Importa tu archivo de estilos
+import styles from './MenuDashboard.module.css'; // Importa tu archivo de estilos como módulo
 
 const MenuDashboard = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,36 +15,33 @@ const MenuDashboard = () => {
 
   return (
     <div>
-      <button 
-        className={`hamburger ${isOpen ? 'open' : ''}`} 
-        onClick={toggleMenu}
-      >
-        &#9776;
-      </button>
 
-      <aside className={`sidebar ${isOpen ? 'open' : ''}`}>
-        <div className="profile">
-          <div className="profile-pic"></div>
+      <aside className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
+        <button className={`${styles.hamburger} ${isOpen ? styles.open : ''}`} onClick={toggleMenu}>
+          &#9776;
+        </button>
+        <div className={styles.profile}>
+          <div className={styles.profilePic}></div>
           <h2>Admin_name</h2>
         </div>
-        <nav className="menu">
+        <nav className={styles.menu}>
           <ul>
-            <li className={isActive('/dashboard') ? 'active' : ''}>
+            <li className={isActive('/dashboard') ? styles.active : ''}>
               <a href='/dashboard'>Dashboard</a>
             </li>
-            <li className={isActive('/dashboard-solicitudes') ? 'active' : ''}>
+            <li className={isActive('/dashboard-solicitudes') ? styles.active : ''}>
               <a href='/dashboard-solicitudes'>Solicitudes</a>
             </li>
-            <li className={isActive('/dashboard-user') ? 'active' : ''}>
+            <li className={isActive('/dashboard-user') ? styles.active : ''}>
               <a href='/dashboard-user'>Usuarios</a>
             </li>
-            <li className={isActive('/dashboard-profit') ? 'active' : ''}>
+            <li className={isActive('/dashboard-profit') ? styles.active : ''}>
               <a href='/dashboard-profit'>Ganancias</a>
             </li>
-            <li className={isActive('/dashboard-config') ? 'active' : ''}>
+            <li className={isActive('/dashboard-config') ? styles.active : ''}>
               <a href='/dashboard-config'>Configuración</a>
             </li>
-            <li className={isActive('/') ? 'active' : ''}>
+            <li className={isActive('/') ? styles.active : ''}>
               <a href='/'>Cerrar sesión</a>
             </li>
           </ul>
