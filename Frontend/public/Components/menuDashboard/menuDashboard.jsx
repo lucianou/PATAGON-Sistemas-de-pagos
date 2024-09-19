a702b748f70b3fcf909521ef94e71e3f82b5e49a
Lukayx <olavarriafernando36@gmail.com>
2024-09-19 03:21:24 -0300

    Cambios al menú Dashboard

--- Frontend/public/Components/menuDashboard/menuDashboard.jsx
+++ Frontend/public/Components/menuDashboard/menuDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom'; // Importa useLocation para obtener la URL actual
 import styles from './MenuDashboard.module.css'; // Importa tu archivo de estilos como módulo
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTachometerAlt, faClipboard, faUsers, faDollarSign, faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTachometerAlt, faClipboard, faUsers, faDollarSign, faCog, faSignOutAlt, faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
 
 const MenuDashboard = ({ toggleMenu, isOpen }) => {
   const location = useLocation(); // Obtén la URL actual  
@@ -21,7 +21,7 @@ const MenuDashboard = ({ toggleMenu, isOpen }) => {
     <div>
       <aside className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
         <button className={`${styles.hamburger} ${isOpen ? styles.open : ''}`} onClick={toggleMenu}>
-          &#9776;
+          <FontAwesomeIcon icon={isOpen ? faTimes : faBars} className={styles.faIcon}/>
         </button>
         <div className={styles.profile}>
           <img className={styles.profilePic} src='../../src/assets/perro.png' alt='Profile'></img>
@@ -30,39 +30,39 @@ const MenuDashboard = ({ toggleMenu, isOpen }) => {
         <nav className={styles.menu}>
           <ul>
             <li className={isActive('/dashboard') ? styles.active : ''}>
-              <FontAwesomeIcon icon={faTachometerAlt} className={styles.faIcon}/> 
               <a href='/dashboard'>
-                Dashboard
+                <FontAwesomeIcon icon={faTachometerAlt} className={styles.faIcon}/> 
+                <span>Dashboard</span>
               </a>
             </li>
             <li className={isActive('/dashboard-solicitudes') ? styles.active : ''}>
-              <FontAwesomeIcon icon={faClipboard} className={styles.faIcon}/>
               <a href='/dashboard-solicitudes'>
-                Solicitudes
+                <FontAwesomeIcon icon={faClipboard} className={styles.faIcon}/>
+                <span>Solicitudes</span>
               </a>
             </li>
             <li className={isActive('/dashboard-user') ? styles.active : ''}>
-              <FontAwesomeIcon icon={faUsers} className={styles.faIcon}/> 
               <a href='/dashboard-user'>
-                Usuarios
+                <FontAwesomeIcon icon={faUsers} className={styles.faIcon}/> 
+                <span>Usuarios</span>
               </a>
             </li>
             <li className={isActive('/dashboard-profit') ? styles.active : ''}>
-              <FontAwesomeIcon icon={faDollarSign} className={styles.faIcon}/> 
               <a href='/dashboard-profit'>
-                Ganancias
+                <FontAwesomeIcon icon={faDollarSign} className={styles.faIcon}/> 
+                <span>Ganancias</span>
               </a>
             </li>
             <li className={isActive('/dashboard-config') ? styles.active : ''}>
-              <FontAwesomeIcon icon={faCog} className={styles.faIcon}/>
               <a href='/dashboard-config'>
-                Configuración
+                <FontAwesomeIcon icon={faCog} className={styles.faIcon}/>
+                <span>Configuración</span>
               </a>
             </li>
             <li className={isActive('/') ? styles.active : ''}>
-              <FontAwesomeIcon icon={faSignOutAlt} className={styles.faIcon}/>
-              <a href='/' onClick={logout}>
-                Cerrar sesión
+              <a href='/' onClick={logout }>
+                <FontAwesomeIcon icon={faSignOutAlt} className={styles.faIcon}/>
+                <span>Cerrar</span>
               </a>
             </li>
           </ul>
