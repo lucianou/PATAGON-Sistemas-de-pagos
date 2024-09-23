f04a74cc4e8ba6dc7d853eadad4dc7672d3a915a
MeatOfJustice <33604227+MeatOfJustice@users.noreply.github.com>
2024-09-23 12:01:08 -0300

    Notificación con sonner (intercambiar archivos)

---
+++ Frontend/src/Pages/Notifications_Sonner.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { toast, Toaster } from 'sonner';
+import { io } from 'socket.io-client';
+import styles from './Notifications.module.css'; // Importar los estilos modulares
+//npm install sonner
+const Notifications = () => {
+  useEffect(() => {
+    const socket = io('http://localhost:3004');
+
+    socket.emit('joinAdmin', 'admin');
+
+    socket.on('newNotification', (message) => {
+      toast(message, {
+        className: styles.toast, // Aplicar el estilo modular a la notificación
+      });
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Toaster position="bottom-right" />
+    </div>
+  );
+};
+
+export default Notifications;
