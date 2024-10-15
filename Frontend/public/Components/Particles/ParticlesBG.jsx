12d810db72f591010e719b0a3656545569027d0a
Vicntea <128265139+Vicntea@users.noreply.github.com>
2024-10-15 17:10:00 -0300

    Agregué comentarios e incorporé una variable setup, para settear la configuración que tendrán las particulas.

--- Frontend/public/Components/Particles/ParticlesBG.jsx
+++ Frontend/public/Components/Particles/ParticlesBG.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim"; // Asegúrate de tener esta dependencia instalada
+import particleConfig from './config/setup';
 
+// Usa particleConfig donde lo necesites
+
+
+// Componente que permite el uso de particulas 
 const ParticlesComponent = React.memo((props) => {
   const [init, setInit] = useState(false);
 
@@ -13,116 +18,13 @@ const ParticlesComponent = React.memo((props) => {
     });
   }, []);
 
+  // Esta función permite traer la configuración la cual tendrá la página con las particulas
   const particlesLoaded = (container) => {
     console.log(container);
   };
 
   const options = useMemo(
-    () => ({
-      background: {
-        color: {
-          value: "#000000", // Cambia aquí el color de fondo
-        },
-      },
-      fpsLimit: 120,
-      interactivity: {
-        events: {
-          onHover: {
-            enable: true,
-            mode: "repulse",
-          },
-        },
-        modes: {
-          grab: {
-            distance: 400,
-          },
-          bubble: {
-            distance: 250,
-            size: 0,
-            duration: 2,
-            opacity: 0,
-            speed: 3,
-          },
-          repulse: {
-            distance: 125,
-            duration: 0.4,
-          },
-          push: {
-            particles_nb: 4,
-          },
-          remove: {
-            particles_nb: 2,
-          },
-        },
-      },
-      particles: {
-        number: {
-          value: 190,
-          density: {
-            enable: true,
-            value_area: 540,
-          },
-        },
-        color: {
-          value: "#02a1f4",
-        },
-        shape: {
-          type: "circle",
-          stroke: {
-            width: 0,
-            color: "#000000",
-          },
-          polygon: {
-            nb_sides: 5,
-          },
-          image: {
-            src: "",
-            width: 100,
-            height: 100,
-          },
-        },
-        opacity: {
-          value: 1,
-          random: true,
-          anim: {
-            enable: true,
-            speed: 1,
-            opacity_min: 0,
-            sync: false,
-          },
-        },
-        size: {
-          value: { min: 1, max: 3 },
-          random: true,
-          anim: {
-            enable: false,
-            speed: 4,
-            size_min: 0.3,
-            sync: false,
-          },
-        },
-        links: {
-          enable: false,
-        },
-        move: {
-          enable: true,
-          speed: 1,
-          direction: "none",
-          random: true,
-          straight: false,
-          outModes: {
-            default: "bounce",
-          },
-          bounce: false,
-          attract: {
-            enable: false,
-            rotateX: 600,
-            rotateY: 600,
-          },
-        },
-      },
-      detectRetina: false,
-    }),
+    () => (particleConfig),
     []
   );
 
