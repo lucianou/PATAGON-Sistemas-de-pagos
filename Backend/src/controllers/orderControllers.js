24d786c076a427a69fcbf6450a034a9c18243745
Fernando Castillo <fernacastillo.perez0607@gmail.com>
2024-10-23 13:17:13 -0300

    Backend ruta y controlador para la creacion de usuarios con roles, por parte del administrador

--- Backend/src/controllers/orderControllers.js
+++ Backend/src/controllers/orderControllers.js
@@ -1,52 +1,3 @@
-<<<<<<< Updated upstream
-import https from 'https';
-
-export const crearOrden = (req, res) => {
-  const { monto, ordenCompra, email } = req.body;
-
-  const data = JSON.stringify({
-    apiKey: process.env.API_KEY,
-    apiSecret: process.env.SECRET_KEY,
-    commerceOrder: ordenCompra,
-    subject: 'Compra en Mi Tienda',
-    amount: monto,
-    email: email,
-    urlReturn: 'https://tuapp.com/retorno',
-    urlConfirmation: 'https://tuapp.com/confirmacion'
-  });
-
-  const options = {
-    hostname: 'sandbox.flow.cl',
-    port: 443,
-    path: '/api/payment/create',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': data.length,
-    },
-  };
-
-  const reqApi = https.request(options, (response) => {
-    let responseBody = '';
-
-    response.on('data', (chunk) => {
-      responseBody += chunk;
-    });
-
-    response.on('end', () => {
-      const responseData = JSON.parse(responseBody);
-      res.json({ urlPago: responseData.urlPago });
-    });
-  });
-
-  reqApi.on('error', (error) => {
-    console.error(error);
-    res.status(500).json({ error: 'Error al generar la orden' });
-  });
-
-  reqApi.write(data);
-  reqApi.end();
-=======
 import axios from 'axios';
 import crypto from 'crypto';
 
@@ -135,5 +86,4 @@ export const confirmPayment = async (req, res) => {
     console.error('Error al confirmar el pago:', error.response?.data || error.message);
     res.status(500).json({ error: 'Error al confirmar el pago' });
   }
->>>>>>> Stashed changes
 };
