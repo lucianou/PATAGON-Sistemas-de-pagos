0c6806cf86dc46e578a1b570ce9cbc4236422837
Vicntea <128265139+Vicntea@users.noreply.github.com>
2024-10-28 12:24:37 -0300

    se modificó y reorganizo not fourd

--- Frontend/src/Pages/client/Docs.jsx
+++ Frontend/src/Pages/client/Docs.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
- 
+import styles from '../../styles/client/Docs.module.css';
 const Docs = ({}) => {
   return (
     <h1>docs</h1>
   )
 }
 
-export default Docs
+export default Docs;
