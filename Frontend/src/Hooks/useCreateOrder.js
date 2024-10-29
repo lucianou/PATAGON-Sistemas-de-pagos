52190c091e2dc6f8d6db9404a2ed1cc59cc8b19d
Fernando Castillo <fernacastillo.perez0607@gmail.com>
2024-10-29 15:18:21 -0300

    Pago hacia el backend
    Implentación de rutas get hacia backend para la implementación de pagos via paypal

--- Frontend/src/Hooks/useCreateOrder.js
+++ Frontend/src/Hooks/useCreateOrder.js
@@ -3,14 +3,14 @@ import { useState } from 'react';
 const useCreateOrder = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const HOST = import.meta.env.VITE_IP;
 
     const createOrder = async (orderData) => {
         setLoading(true);
         setError(null);
 
         try {
-            const response = await fetch('http://localhost:3003/api/command/create-payment', {
-
+            const response = await fetch(`http://${HOST}:3003/api/command/create-payment`, {	
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
