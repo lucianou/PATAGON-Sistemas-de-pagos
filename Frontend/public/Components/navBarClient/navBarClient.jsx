2eadf8d10d4c36f79bf6b82dc02c91f4df6073d1
Lukayx <olavarriafernando36@gmail.com>
2024-11-05 16:16:25 -0300

    Agregado valores al main

--- Frontend/public/Components/navBarClient/navBarClient.jsx
+++ Frontend/public/Components/navBarClient/navBarClient.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import styles from "./navBarClient.module.css";
-import logo from "../../../src/assets/patagon-logo-text-color.png";
-import wave from "../../../src/assets/wave.svg";
 
 const navBar = () => {
+  const isActive = (path) => location.pathname === path;
+  
   return (
     <>
       <div className={styles.header}>
         {/* <img src={wave} alt="wave" className={styles.wave}/> */}
         <div className={styles.sectionIzq}>
-          <a href='/mainClient' tabIndex='-1'>
+          <a href='/mainClient' className={isActive("/mainClient") ? styles.active : ''} tabIndex='-1'>
             Home
           </a>
-          <a href='/UsInfo' tabIndex='-1'>
+          <a href='/UsInfo' className={isActive("/UsInfo") ? styles.active : ''} tabIndex='-1'>
             Nosotros
           </a>
-          <a href='/Bolsa' tabIndex='-1'>
+          <a href='/Bolsa' className={isActive("/Bolsa") ? styles.active : ''} tabIndex='-1'>
             Bolsas
           </a>
-          <a href='/Docs' tabIndex='-1'>  
+          <a href='/Docs' className={isActive("/Docs") ? styles.active : ''} tabIndex='-1'>  
             Docs
           </a>
         </div>
