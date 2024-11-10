15aed975c7d0e8d2d80c0c3ea18d071a47513dcd
Fernando Castillo <fernacastillo.perez0607@gmail.com>
2024-11-10 11:08:57 -0300

    Documentación del sistema
    Documentación de la base de datos junto con sus modelos, diagramas de secuencias y casos de prueba

---
+++ Docs/DB/Modelos/user.js
@@ -0,0 +1,55 @@
+import { DataTypes, Sequelize } from 'sequelize';
+import sequelize from '../config/sequelize.js';
+
+const User = sequelize.define('User', {
+  ID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  rol: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  fecha_ingreso: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.literal('CURRENT_DATE'),
+    allowNull: false,
+  },
+  refresh_token: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  hours_remaining: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  }, {
+  timestamps: false,
+  tableName: "Users",
+}
+);
+
+export default User;
