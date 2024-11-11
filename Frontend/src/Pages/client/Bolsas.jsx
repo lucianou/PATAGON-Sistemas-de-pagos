5101ea948aa962ed6f03337d134b7ba2155eab51
Lukayx <olavarriafernando36@gmail.com>
2024-11-10 23:40:37 -0300

    se agregaron las bolsas a la pagina bolsas.jsx y cambio navBar

--- Frontend/src/Pages/client/Bolsas.jsx
+++ Frontend/src/Pages/client/Bolsas.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import styles from '../../styles/client/Bolsas.module.css';
 import NavBar from "../../../public/Components/navBarClient/navBarClient";
+import DashboardBolsasUser from "../../Hooks/useDashboardBolsasUser.js";
+import Card from '../../../public/Components/Tarjeta/Card.jsx';
 
 const Bolsas = ({}) => {
+  const { bolsas, loading, error } = DashboardBolsasUser();
+
   return (
   <div className= {styles.container}>
     <NavBar></NavBar>
     <section className= {styles.section1}>
       {/* aquí dejen las bolsaas cabroooooooos */}
       <h1>bolsas</h1>
+      <div className={styles.dashboardWidgets}>
+        {loading ? (
+            <div className={styles.spinner}></div>
+          ) : error ? (
+            <p>Error al cargar bolsas: {error}</p>
+          ) : bolsas && bolsas.length > 0 ? (  
+            bolsas.map((bolsa, index) => {
+              const delay = `${index * 100}ms`; 
+              return (
+                <Card
+                  key={index} 
+                  nombre={bolsa.nombre}
+                  tiempo={bolsa.time}
+                  precio={bolsa.precio}
+                  detalles={bolsa.detalles} 
+                  delay={delay} 
+                  ID={bolsa.ID}
+                />
+              );
+            })
+          ) : (
+            <p>No hay bolsas disponibles.</p>
+        )}
+      </div>
     </section>
+
     <section className={styles.section2}></section>
   </div>
   )
