504059d6bfa7ec783c7c69560542227644df17ee
lucianouu <luciano.espinoza@alumnos.uach.cl>
2024-11-26 15:32:07 -0300

    responsividad 1

--- Frontend/public/Components/navBarClient/navBarClient.jsx
+++ Frontend/public/Components/navBarClient/navBarClient.jsx
@@ -1,43 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./navBarClient.module.css";
-import logo from "../../../src/assets/SoloLogo_Patagon.png"
+import logo from "../../../src/assets/SoloLogo_Patagon.png";
 
-const navBar = () => {
+const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
   const isActive = (path) => location.pathname === path;
-  
+
   return (
-    <>
-      <div className={styles.header}>
-        {/* <img src={wave} alt="wave" className={styles.wave}/> */}
-        <div className={styles.sectionIzq}>
+    <div className={styles.header}>
+      <div className={styles.sectionIzq}>
         <img src={logo} alt="logo" className={styles.logo} />
-        <div className={styles.btnGroup}>
-          <a href='/mainClient' className={isActive("/mainClient") ? styles.active : ''} tabIndex='-1'>
+        <div className={`${styles.hamburger} ${menuOpen ? styles.open : ""}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className={`${styles.navMenu} ${menuOpen ? styles.show : ""}`}>
+          <a href="/mainClient" className={isActive("/mainClient") ? styles.active : ""}>
             Home
           </a>
-        </div>
-        <div className={styles.btnGroup}>
-          <a href='/about-us' className={isActive("/about-us") ? styles.active : ''} tabIndex='-1'>
+          <a href="/about-us" className={isActive("/about-us") ? styles.active : ""}>
             Nosotros
           </a>
-        </div>
-        <div className={styles.btnGroup}>
-          <a href='/docs' className={isActive("/docs") ? styles.active : ''} tabIndex='-1'>  
+          <a href="/docs" className={isActive("/docs") ? styles.active : ""}>
             Documentación
           </a>
         </div>
-        </div>
-        <div className={styles.userDiv}>
-          <a className={styles.user}>Usuario</a>
-          <ul>
-            <li><a href="/account/profile">Mi Perfil</a></li>
-            <li><a href="/account/purchase-history">Historial de compras</a></li>
-            <li><a href="/">Cerrar sesión</a></li>
-          </ul>
-        </div>
       </div>
-    </>
+      <div className={styles.userDiv}>
+        <a className={styles.user}>Usuario</a>
+        <ul>
+          <li><a href="/account/profile">Mi Perfil</a></li>
+          <li><a href="/account/purchase-history">Historial de compras</a></li>
+          <li><a href="/">Cerrar sesión</a></li>
+        </ul>
+      </div>
+    </div>
   );
-}
+};
 
-export default navBar;
+export default NavBar;
